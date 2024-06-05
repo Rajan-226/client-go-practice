8de49b0af327248e6f0d7e3846c8f6acb6f20d2c
Rajan Sharma <rajansharma842214@gmail.com>
2024-06-05 13:36:29 +0530

    Edit deployment tag

--- utils/utils.go
+++ utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -18,4 +19,39 @@ func PrintPods(ctx context.Context, clientSet *kubernetes.Clientset) {
 	for index, pod := range pods.Items {
 		fmt.Printf("Pod %d : %+v\n", index, pod.Name)
 	}
+
+	fmt.Println("Successfully Printed all pods!!")
+}
+
+func EditDeploymentImageTag(ctx context.Context, clientSet *kubernetes.Clientset, deploymentName string, tag string) {
+	deployment, err := clientSet.AppsV1().Deployments("ns-one").Get(ctx, deploymentName, metav1.GetOptions{})
+
+	if err != nil {
+		fmt.Println(err.Error(), " while getting the pods from clientset")
+	}
+
+	containers := deployment.Spec.Template.Spec.Containers
+	for i := range containers {
+		containers[i].Image = updateImageTag(containers[i].Image, tag)
+	}
+
+	_, err = clientSet.AppsV1().Deployments("ns-one").Update(ctx, deployment, metav1.UpdateOptions{})
+	if err != nil {
+		fmt.Println(err.Error() + " while updating the image tag of deployment")
+	}
+
+	fmt.Printf("Successfully Changed Image of %s deployment to %s!!\n", deploymentName, tag)
+}
+
+func updateImageTag(image string, newTag string) string {
+	if colonIndex := strings.LastIndex(image, ":"); colonIndex != -1 {
+		return image[:colonIndex+1] + newTag
+	}
+
+	return image + ":" + newTag
+}
+
+func Temp() {
+	// informers.NewSharedInformerFactory().Core().V1().Pods().Informer().AddEventHandler()
+	// cache.ResourceEventHandlerFuncs
 }
